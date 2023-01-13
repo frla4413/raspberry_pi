93f7573b75d7d072c6a7633bdba432767d22c3a5
fredriklauren <fredriklauren1@gmail.com>
2023-01-13 23:33:31 +0100

    minor change

--- api/webserver/app.py
+++ api/webserver/app.py
@@ -22,20 +22,35 @@ def sensors():
     template_data = json.loads(data_base.get_last_data())
     return render_template('sensors.html', **template_data)
 
-@app.route("/plots")
-def make_plot():
+@app.route("/humidity")
+def make__humidity_plot():
 
     data = json.loads(data_base.get_temp_humidity())
     df = pd.DataFrame(data)
 
-    fig = px.line(df, x="Time", y="Humidity")
+    fig = px.line(df, x="Time", y=df.columns[1:3])
     fig.show()
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     header="Humidity at home"
     description = """
     Wow! Sure looks humid enough!
     """
-    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+    return render_template('plot.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route("/temp")
+def make__temperature_plot():
+
+    data = json.loads(data_base.get_temp_humidity())
+    df = pd.DataFrame(data)
+
+    fig = px.line(df, x="Time", y="Temp")
+    fig.show()
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Temperature at home"
+    description = """
+    Wow! Sure looks hot in here!
+    """
+    return render_template('plot.html', graphJSON=graphJSON, header=header,description=description)
 
 
 if __name__ == "__main__":
